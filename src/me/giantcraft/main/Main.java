1077d8a4b803086d5d6bf48f5f28e0cb07139360
Loek-GamePc <Loek-GamePc@DESKTOP-0J8AVB7>
2020-05-08 20:46:31 +0200

    added RitualWaves config

--- src/me/giantcraft/main/Main.java
+++ src/me/giantcraft/main/Main.java
@@ -10,25 +10,25 @@ public class Main extends JavaPlugin {
 
 	// Global game variables
 	public ConfigManager ConfigManager;
-	
-	
+
 	@Override
 	public void onEnable() {
 		// TODO: Loading in configuration file
 		ConfigManager = new ConfigManager();
 		ConfigManager.setup();
 		
-		
 		// TODO: Loading commands
 		this.getCommand("gchelp").setExecutor(new Cmd_Help()); // Loading the 'help' command.
 		getLogger().info(Color.GREEN + "Plugin has been enabled!"); // Let the user know that the plugin is enabled!
 	}
-	
 
 	@Override
 	public void onDisable() {
+		
 		ConfigManager.saveCustomMobs();
+		ConfigManager.saveWaves();
 		getLogger().info(Color.RED + "Plugin has been disabled!"); // Saying bye to you!
+	
 	}
 
 }
