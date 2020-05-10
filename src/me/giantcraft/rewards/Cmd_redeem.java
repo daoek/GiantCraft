53cc94f78df81eab1f4c360c36560f1b0dd2d320
marco <marco@MRCZ-01>
2020-05-10 16:09:38 +0200

    Created the first inventory. Event not working in that class

---
+++ src/me/giantcraft/rewards/Cmd_redeem.java
@@ -0,0 +1,26 @@
+package me.giantcraft.rewards;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Cmd_redeem implements CommandExecutor {
+
+	GuiReward rewards = new GuiReward();
+	
+	// The full code for this command
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if (sender instanceof Player) {			// If the command is executed by the player
+			Player player = (Player) sender;
+			
+			rewards.openInv(player);
+			player.sendMessage("Opening Rewards menu.");
+		}
+		
+		return false;		// Return something.. (You can leave this at 'false')
+	}
+
+}
