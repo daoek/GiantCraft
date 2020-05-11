103b20d4461bff3466f1dafcc999c5d749eb2f72
Loek-GamePc <Loek-GamePc@DESKTOP-0J8AVB7>
2020-05-11 21:07:09 +0200

    h

--- src/me/giantcraft/main/Events.java
+++ src/me/giantcraft/main/Events.java
@@ -3,6 +3,8 @@ package me.giantcraft.main;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Events implements Listener{
@@ -19,5 +21,21 @@ public class Events implements Listener{
 	{
 		event.getPlayer().sendMessage(ChatColor.BLUE + "Welcome to the server! Enjoy!!!");
 	}
-
+	
+	@EventHandler
+	public void onBreakBlock(BlockBreakEvent event)
+	{
+		//main.custommobs.SpawnCustomMob("creeper", event.getBlock().getLocation().add(0,1,0), event.getPlayer().getWorld());
+	}
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		//Player player = event.getPlayer();
+		
+		if(event.getMessage() == "gold") {
+			for(int i = 0; i<100; i++) {
+				//main.custommobs.SpawnCustomMob("creeper", player.getLocation().add(0, 50, 0), event.getPlayer().getWorld());
+			}
+		}
+	}
 }
