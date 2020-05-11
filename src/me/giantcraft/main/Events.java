747d9704f739511097c6d6fe3f68f868c377e801
marco <marco@MRCZ-02>
2020-05-11 12:52:51 +0200

    added extra event

--- src/me/giantcraft/main/Events.java
+++ src/me/giantcraft/main/Events.java
@@ -1,9 +1,11 @@
 package me.giantcraft.main;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Events implements Listener{
@@ -25,5 +27,16 @@ public class Events implements Listener{
 	public void onBreakBlock(BlockBreakEvent event)
 	{
 		main.custommobs.SpawnCustomMob("creeper", event.getBlock().getLocation().add(0,1,0), event.getPlayer().getWorld());
+	}
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		Player player = event.getPlayer();
+		
+		if(event.getMessage() == "gold") {
+			for(int i = 0; i<100; i++) {
+				main.custommobs.SpawnCustomMob("creeper", player.getLocation().add(0, 50, 0), event.getPlayer().getWorld());
+			}
+		}
 	}
 }
